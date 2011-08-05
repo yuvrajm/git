704c3359caf3054469c389a8bc84adb36934e728
Pascal Obry <pascal@obry.net>
2011-08-05 18:10:14 +0200

    On Cygwin support both UNIX and DOS style path-names
    In fact Cygwin supports both, so make Git agree with this.
    The failing case is when a file is committed in a sub-dir of the
    repository using a log message from a file specified with a DOS
    style path-name. To reproduce:
    
       $ cd src
       $ git commit -F c:\tmp\log.txt file.c
       fatal: could not read log file 'src/c:\tmp\log.txt': No such file \
       or directory.
    
    Signed-off-by: Pascal Obry <pascal@obry.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- compat/cygwin.h
+++ compat/cygwin.h
@@ -7,3 +7,6 @@ extern stat_fn_t cygwin_lstat_fn;
 
 #define stat(path, buf) (*cygwin_stat_fn)(path, buf)
 #define lstat(path, buf) (*cygwin_lstat_fn)(path, buf)
+
+#define has_dos_drive_prefix(path) (isalpha(*(path)) && (path)[1] == ':')
+#define is_dir_sep(c) ((c) == '/' || (c) == '\\')
