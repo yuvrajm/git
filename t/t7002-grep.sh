469e961af2d970d15740138f477aab00708698ae
Junio C Hamano <gitster@pobox.com>
2011-09-28 13:54:12 -0700

    grep: rename --exclude to --exclude-standard
    This better matches what ls-files does.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- t/t7002-grep.sh
+++ t/t7002-grep.sh
@@ -463,7 +463,7 @@ test_expect_success 'outside of git repository' '
 		export GIT_CEILING_DIRECTORIES &&
 		cd non/git &&
 		test_must_fail git grep o &&
-		git grep --no-index --exclude o >../actual.full &&
+		git grep --no-index --exclude-standard o >../actual.full &&
 		test_cmp ../expect.full ../actual.full &&
 
 		{
@@ -503,7 +503,7 @@ test_expect_success 'inside git repository but with --no-index' '
 		git grep --no-index o >../actual.full &&
 		test_cmp ../expect.full ../actual.full &&
 
-		git grep --no-index --exclude o >../actual.unignored &&
+		git grep --no-index --exclude-standard o >../actual.unignored &&
 		test_cmp ../expect.unignored ../actual.unignored &&
 
 		cd sub &&
